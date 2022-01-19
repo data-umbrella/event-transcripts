a149ecff20589f07e2e9f74cb99b238d7176c69c
Reshama Shaikh <reshama.stat@gmail.com>
2022-01-19 09:18:36 -0500

    Create 42_gonzalo_ghactions.m

---
+++ misc/42_gonzalo_ghactions.m
@@ -0,0 +1,47 @@
+```text
+## Upcoming Events
+Join our Meetup group for more events!
+https://www.meetup.com/data-umbrella
+
+Gonzalo Peña-Castellanos: Automating Workflows with GitHub Actions
+
+## Key Links
+- Transcript: https://github.com/data-umbrella/event-transcripts/blob/main/2022/42-gonzalo-github-actions.md
+- Meetup Event: https://www.meetup.com/data-umbrella/events/282772806/
+- Video:  https://youtu.be/d48WGkePFq0
+- GitHub repo:
+
+
+## Agenda
+00:00:00 Reshama introduces Data Umbrella
+00:00:00 
+00:00:00  
+00:00:00  
+00:00:00  
+00:00:00  
+00:00:00  
+00:00:00   
+00:00:00  
+00:00:00  
+00:00:00  
+00:00:00  
+00:00:00  
+
+ 
+## Event
+Learn about GitHub Actions and how they can accelerate your application development workflows. A workflow is a configurable automated process made up of one or more jobs. We will learn how to configure and use workflows existing current actions and start looking at how an action can be developed from scratch.
+
+## Pre-reqs
+Beginner knowledge of Python. YAML knowledge. Git/GitHub knowledge.
+
+## Prep Work
+Be comfortable with git, GitHub, making pushes to branches and pull requests.
+    
+## About the Speaker
+I am a Colombian Civil Engineer with an MSc. in Hydroinformatics and an MSc. in Sanitary Engineering that discovered a passion for Software Development and Data Science which led me to collaborate in different open source projects and to eventually join Anaconda, Inc (formerly known as Continuum Analytics) and currently Quansight, Inc as Senior Software Engineer, and Data Scientist (in development). I am an active member of the open source community contributing to different projects and a leader in the Python Colombia Community helping shape and organize communities such as Django Girls Colombia, Python Bucaramanga, Python Colombia and the national and Latin American Python Conferences, PyCon Colombia (2018, 2019, 2020) and SciPy Latam (2019, 2021) respectively. I am currently a core developer with Project Jupyter and Project napari. I am the author of the setup-miniconda Github Action to help automate conda environment and workflows.
+
+LinkedIn: https://www.linkedin.com/in/goanpeca
+Twitter: https://twitter.com/goanpeca
+GitHub: https://github.com/goanpeca
+
+```
